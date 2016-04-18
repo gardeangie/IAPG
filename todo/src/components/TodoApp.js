f3916cb9459ab2dcbf86d94a0c12666b43129cf1
gardeangie <gardeangie@gmail.com>
2016-04-18 17:36:21 +0800

    first todo app changes

---
+++ todo/src/components/TodoApp.js
@@ -0,0 +1,24 @@
+/**
+ * Created by student1 on 4/18/2016.
+ */
+/**
+ * Created by albertoclarit on 4/18/16.
+ */
+import React from 'react';
+
+export default  class TodoApp extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+
+    render(){
+
+        return (
+            <div>
+            <h1>Welcome to ReactJS</h1>
+        </div>
+    )
+    }
+}
---
+++ todo/src/components/run.js
@@ -0,0 +1,10 @@
+/**
+ * Created by student1 on 4/18/2016.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TodoApp from './TodoApp'
+
+
+ReactDOM.render(<TodoApp/>, document.getElementById('app'));
